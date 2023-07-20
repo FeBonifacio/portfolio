e1b9226923443ce52353edea88d3f421a24a09a2
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-07-20 16:29:27 -0300

    Criando ferramenta para mobile

--- js/script.js
+++ js/script.js
@@ -16,6 +16,10 @@ function escrevendoLetra () {
 }
 
 const menu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .nav-primary');
+
+
 menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-x');
+    menu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
 });
