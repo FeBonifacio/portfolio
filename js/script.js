6091e57821e48a7a303a7da62d84e65996b6596d
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-07-20 14:50:18 -0300

    Fazerndo a responsividade de alguns elementos.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-function ativaLetra(element) {
-    const arrText = element.innerHTML.split('');
-    element.innerText = '';
-    arrText.forEach((letra, i)=>{
-        setTimeout(() => {
-            element.innerHTML += letra;
-        }, 75 * i);
-    });
+//Letra piscando
+function escrevendoLetra () {
+    function ativaLetra(element) {
+        const arrText = element.innerHTML.split('');
+        element.innerText = '';
+        arrText.forEach((letra, i)=>{
+            setTimeout(() => {
+                element.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
+    
+    const titulo = document.querySelector('.typing');
+    
+    ativaLetra(titulo)
 }
 
-const titulo = document.querySelector('.typing');
-
-ativaLetra(titulo)
-
+const menu = document.querySelector('.fa-bars');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-x');
+});
