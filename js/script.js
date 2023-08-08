57ff36f62e763163f470668676decc2a13241e79
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-08-08 15:45:59 -0300

    Finalizando Porft.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 //Letra piscando
 function escrevendoLetra () {
     function ativaLetra(element) {
@@ -15,11 +17,113 @@ function escrevendoLetra () {
     ativaLetra(titulo)
 }
 
-const menu = document.querySelector('.fa-bars');
-const navMenu = document.querySelector('header .nav-primary');
+escrevendoLetra();
 
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-x')
-    navMenu.classList.toggle('ativado')
+function menuMobol() {
+    const menu = document.querySelector('.fa-bars');
+    const navMenu = document.querySelector('header .nav-primary');
+
+
+        menu.addEventListener('click', () => {
+        menu.classList.toggle('fa-x');
+        navMenu.classList.toggle('ativado');
+    });
+}
+
+function sobreMin() {
+
+    const divEducation = document.querySelectorAll('.education-content div');
+    const liEducation = document.querySelectorAll('.education-content ul li');
+
+
+    divEducation[0].classList.add('ativo');
+    liEducation[0].classList.add('ativo');
+
+    function slideShow(index) {
+    divEducation.forEach((div) => {
+        div.classList.remove('ativo');
+    });
+
+    liEducation.forEach((button) => {
+        button.classList.remove('ativo');
+    });
+
+    divEducation[index].classList.add('ativo');
+    liEducation[index].classList.add('ativo');
+    }
+
+
+    liEducation.forEach((e, index) => {
+        e.addEventListener('click', () => {
+        slideShow(index);
+        });
+    });
+}
+
+sobreMin();
+
+const listAll = document.querySelectorAll('.projects-storage ul li');
+const buttonGeral = document.querySelectorAll('.projects-models ul li');
+const buttonAll = document.querySelector('.projects-models .all');
+
+function removeClick(index) {
+    buttonGeral.forEach((item) => {
+        item.classList.remove('ativo');
+    });
+    buttonGeral[index].classList.add('ativo');
+}
+
+
+buttonGeral.forEach((event, index) => {
+    event.addEventListener('click', () => {
+        removeClick(index)
+    })
+})
+
+function showLista(lista, buttom = "web") {
+    lista.forEach((item) => {
+        item.classList.remove('ativo');
+    })
+
+    if (buttom == 'web') {
+        lista[0].classList.add('ativo');
+        lista[1].classList.add('ativo');
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+    }
+
+    if (buttom == 'mobile') {
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+
+    if (buttom == 'all') {
+        lista[0].classList.add('ativo');
+        lista[1].classList.add('ativo');
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+}
+
+buttonGeral.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let currentButton = e.target;
+        if(currentButton.classList.contains("all")){
+            showLista(listAll);
+
+        } if(currentButton.classList.contains("web")){
+            showLista(listAll, "web")
+        }
+
+        if(currentButton.classList.contains("mobile")){
+            showLista(listAll, "mobile")
+        }
+    });
 });
